0c55381094d4e10aa070b74b1e49d1081fcc7cf1
Moy1991 <30806194+Moy1991@users.noreply.github.com>
2017-08-11 14:59:19 -0500

    lab 3

---
+++ Day03_Aug11/Lab/lab3ANSWERS.py
@@ -0,0 +1,48 @@
+def hasNumber(inputString):
+    return any(char.isdigit() for char in inputString)
+
+
+#Hello. to HELLO!
+def shout(txt):
+    if txt[-1] == ".":
+        new_txt = txt.upper()
+        new_txt = new_txt[:-1]
+        new_txt = new_txt + "!"
+        return new_txt
+    elif hasNumber(txt) == True:
+        raise Exception, "Cannot include numbers"
+    else:
+        raise Exception, "Must end with a period"
+
+
+
+
+# Name to emaN
+def reverse(txt):
+    txt = str(txt)
+    n1_txt = txt.split()
+    if len(n1_txt) == 1:
+        new_txt = txt[::-1]
+    else:
+        raise Exception, "Must be one word"
+    return str(new_txt)
+
+
+
+# Hello world! to world! Hello
+def reversewords(txt):
+    if hasNumber(txt) == True:
+        raise Exception, "Cannot include numbers"
+    else:
+        new_txt = txt.split()
+        new_txt = new_txt[::-1]
+        new_txt = " ".join(new_txt)
+        return new_txt
+
+# Hello world! to !dlrow olleH
+def reversewordletters(txt):
+    if hasNumber(txt) == True:
+        raise Exception, "Cannot include numbers"
+    else:
+        new_txt = txt[::-1]
+        return new_txt
---
+++ Day03_Aug11/Lab/testlab3.py
@@ -0,0 +1,18 @@
+import unittest
+from lab3ANSWERS import *
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_shout(self):
+        self.assertEqual(shout('Hello.'), 'HELLO!')
+    def test_reverse(self):
+        self.assertEqual(reverse('Name'), 'emaN')
+    def test_reversewords(self):
+        self.assertEqual(reversewords('Hello world!'), 'world! Hello')
+    def test_reversewordletters(self):
+        self.assertEqual(reversewordletters('Hello world!'), '!dlrow olleH')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
