e6eb88215fe59a7b9f02d4190c2fa862d0f4e594
Moy1991 <30806194+Moy1991@users.noreply.github.com>
2017-08-22 07:09:09 -0500

    getting started

---
+++ Homeworks/HW4/SortingAlgMOY.py
@@ -0,0 +1,12 @@
+## HW4
+
+# 2 different sorting algorithms (ones from class just with different complexities)
+# O(n^2) and O(nlogn)
+
+#graph with -Vertical axis in time - Horizontal axis is N size of set to sort - one line for each algorithm = label everything
+# https://visualgo.net/bn/sorting
+# https://betterexplained.com/articles/sorting-algorithms/#Radix_sort_BestAvgWorst_ON
+
+# Graphing in python https://plot.ly/python/
+
+
