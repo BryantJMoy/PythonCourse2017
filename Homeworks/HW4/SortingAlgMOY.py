de56110adc37ecd8747b2c61a484f96379c969bf
Moy1991 <30806194+Moy1991@users.noreply.github.com>
2017-08-25 08:36:02 -0500

    final commit

--- Homeworks/HW4/SortingAlgMOY.py
+++ Homeworks/HW4/SortingAlgMOY.py
@@ -8,7 +8,7 @@
 # https://betterexplained.com/articles/sorting-algorithms/#Radix_sort_BestAvgWorst_ON
 
 # Graphing in python https://plot.ly/python/
-
+#py.show() 
 #bubble sort
 #x=[4,2,1]
 
@@ -21,6 +21,9 @@
 #BubbleSort
 
 import random
+import timeit
+import pylab as py
+
 random_l = random.sample(range(10), 10)
 
 listn= [2,1,3]
@@ -35,23 +38,6 @@ BubbleSort(random_l)
 BubbleSort(listn)
 
 
-
-#Insert sort
-
-def InsertSort(listn):
-	for index in range(1,len(listn)):
-		current = listn[index]
-		while index >= 1 and listn[index-1]>current:
-			listn[index]=listn[index-1]
-			index+=-1
-		listn[index] = current
-	return listn
-InsertSort(random_l)
-
-
-# Not differect complexities
-
-# selection sort
 
 
 def SelectionSort(listn):
@@ -69,7 +55,49 @@ def SelectionSort(listn):
 
 # https://stackoverflow.com/questions/8220801/how-to-use-timeit-module
 
-import timeit
+
+def simulation(n,it):
+    t_BubbleSort = []
+    t_SelectionSort = []
+    for i in range(it):
+        sim = random.sample(range(n),n)
+        start_time = timeit.default_timer() 
+        BubbleSort(sim)
+        mid_time = timeit.default_timer() 
+        SelectionSort(sim)    
+        end_time = timeit.default_timer()
+        t_BubbleSort.append(mid_time - start_time)
+        t_SelectionSort.append(end_time - mid_time)
+    return sum(t_BubbleSort)/len(t_BubbleSort), sum(t_SelectionSort)/len(t_SelectionSort) #average
+
+
+
+
+t_BubbleSort = []
+t_SelectionSort = []
+
+
+# 1-99
+
+
+for i in range(2,100):
+    all_times = simulation(i,10) 
+    t_BubbleSort.append(all_times[0])
+    t_SelectionSort.append(all_times[1])
+
+
+
+def MakeFigure(BubbleX1,BubbleY1,SelectionX1,SelectionY2):
+    py.plot(BubbleX1, BubbleY1, label = 'BubbleSort')
+    py.plot(SelectionX1, SelectionY2, label = 'SelectionSort')
+    py.xlabel('Size of Sample')
+    py.ylabel('Average Time')
+    py.legend()
+
+
+
+MakeFigure(range(2,100), t_BubbleSort, range(2,100), t_SelectionSort)
+py.savefig('MoyGraphSort.png')
 
 
 
